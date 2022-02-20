6beb5e419b74cbfbea40dbddc87a378dc465e8cd
shubh-2012 <77185813+shubh-2012@users.noreply.github.com>
2022-02-20 10:20:59 +0530

    Day 2- Recursion
    Recursion

---
+++ factorial.py
@@ -0,0 +1,19 @@
+# factorial of a number
+print('insert a number')
+var = int(input())
+
+def factorial(n):
+    if n < 0:
+        return 0
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        fact = 1
+        while( n > 1 ):
+            fact *= n
+            n -= 1
+
+    return fact
+
+print('factorial of the number is : ',factorial(var))
+
